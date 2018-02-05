22f21f71be911976b371dac72249059eca2c3109
xionggai <hustphysics@hotmail.com>
2018-02-05 20:24:12 +0800

    add position for step

--- src/index.js
+++ src/index.js
@@ -2,19 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// class Square extends React.Component {
-
-//     render() {
-//       return (
-//         <button className="square" onClick={() => this.props.onClick()}>
-//           {this.props.value}
-//         </button>
-//       );
-//     }
-//   }
-
   function Square(props) {
-
     return (
         <button className="square" onClick={props.onClick}>
           {props.value}
@@ -55,19 +43,19 @@ import './index.css';
   }
   
   class Game extends React.Component {
-    
+
     constructor(props) {
         super(props);
         this.state=  {
             history: [{
-                squares: Array(9).fill(null)
+                squares: Array(9).fill(null),
+                index: NaN
             }],
             xIsNext: true
         };
     }
 
     hanldeClick(index) {
-
         const history = this.state.history;
         const current = history[history.length - 1];
         const squares = current.squares.slice();
@@ -77,26 +65,28 @@ import './index.css';
         const value = this.state.xIsNext? "X": "O";
         squares[index] = value;
         this.setState({
-            history: history.concat([{squares: squares}]),
+            history: history.concat([{squares: squares, index}]),
             xIsNext: !this.state.xIsNext
         });
     }
 
     justTo(move) {
-
         const history = this.state.history.slice(0, move + 1);
         const xIsNext = (history.length % 2) === 1;
         this.setState({history, xIsNext});
     }
 
     render() {
-      
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
-            const desc = move? "Go to move#" + move : "Go to game start";
+            const index = step.index;
+            const row = Math.floor(index / 3);
+            const col = index % 3;
+            const position = !isNaN(index)? `(${row}, ${col})` : "";
+            let desc = `${move? "Go to move #" + move : "Go to game start"} ${position}`;
             return (
                 <li key={move}>
                     <button onClick={() => this.justTo(move)}>{desc}</button>
